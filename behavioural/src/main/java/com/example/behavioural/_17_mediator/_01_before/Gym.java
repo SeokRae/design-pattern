253bc7a5bf69ad25eaaf1ba985fdc6c596cad9d0
seokrae <kslbsh@gmail.com>
2021-11-14 19:52:42 +0900

    design-pattern-17 메디에이터 패턴

---
+++ behavioural/src/main/java/com/example/behavioural/_17_mediator/_01_before/Gym.java
@@ -0,0 +1,10 @@
+package com.example.behavioural._17_mediator._01_before;
+
+public class Gym {
+
+    private CleaningService cleaningService;
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+}
