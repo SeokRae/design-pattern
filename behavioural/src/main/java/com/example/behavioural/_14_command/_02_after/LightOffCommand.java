57c0ac1eedb0bc6e1330c524ee8e63206157d01f
seokrae <kslbsh@gmail.com>
2021-11-14 19:39:22 +0900

    design-pattern-14 커맨드 패턴

---
+++ behavioural/src/main/java/com/example/behavioural/_14_command/_02_after/LightOffCommand.java
@@ -0,0 +1,22 @@
+package com.example.behavioural._14_command._02_after;
+
+import com.example.behavioural._14_command._01_before.Light;
+
+public class LightOffCommand implements Command {
+
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+
+    @Override
+    public void undo() {
+        new LightOnCommand(this.light).execute();
+    }
+}
