babf351c0f2e7db280abc1209e61670e82183de8
seokrae <kslbsh@gmail.com>
2021-11-14 20:11:15 +0900

    [design-pattern-20] 상태 패턴

---
+++ behavioural/src/main/java/com/example/behavioural/_20_state/_02_after/Student.java
@@ -0,0 +1,30 @@
+package com.example.behavioural._20_state._02_after;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Student {
+
+    private final String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    private final Set<OnlineCourse> onlineCourses = new HashSet<>();
+
+    public boolean isAvailable(OnlineCourse onlineCourse) {
+        return onlineCourses.contains(onlineCourse);
+    }
+
+    public void addPrivate(OnlineCourse onlineCourse) {
+        this.onlineCourses.add(onlineCourse);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
